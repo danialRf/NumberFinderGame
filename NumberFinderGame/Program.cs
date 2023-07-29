07b16eb4160c7036de02dfca5a0f9fe9d6b6e0f9
Danial <D.roshanfekr@gmial.com>
2023-07-29 23:19:22 +0330

    initial of the game

---
+++ NumberFinderGame/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("guess a number between 0 and 10000");
+int min = 0;
+int max = 10000;
+int guess = (min+max)/2;
+
+for (int i = 0; i < guess; i++)
+{
+    Console.WriteLine($"is your answer bigger than {guess}");
+    string answer = Console.ReadLine();
+
+    if (answer == "Y")
+    {
+        min = guess;
+        guess = (min+max)/2;
+    }
+    else if (answer == "N")
+    {
+        Console.WriteLine($"is your answer lower than {guess}?");
+        answer = Console.ReadLine();
+        if (max==min||max == guess||min == guess|| answer == "N")
+        {
+            Console.WriteLine($"your number is {guess}");
+            break;
+        }
+        else
+        {
+            max = guess;
+            guess = (max + min) / 2;
+        }
+        
+    }
+}
